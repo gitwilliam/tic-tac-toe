8b0605ba74aa63171db4fe0a36d95bfd8e876787
Bill Harding <william.paul.harding@gmail.com>
2018-07-11 22:10:31 -0500

    Install Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAb14ViwQbnG-MLd3w6jSyVXo4jOpZDPgs",
+    authDomain: "test-tic-tac-toe-6cf88.firebaseapp.com",
+    databaseURL: "https://test-tic-tac-toe-6cf88.firebaseio.com",
+    projectId: "test-tic-tac-toe-6cf88",
+    storageBucket: "test-tic-tac-toe-6cf88.appspot.com",
+    messagingSenderId: "1065065521788"
+  }
 };
 
 /*
