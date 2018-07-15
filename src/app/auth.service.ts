82bbcebce9e7fd448f1ed9e5dbfdd21f3677ae8b
Bill Harding <william.paul.harding@gmail.com>
2018-07-14 22:31:13 -0500

    Add authentication to AuthService

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthService {
+  private authenticated: Observable<boolean>;
+  private userId: string;
 
-  constructor() { }
+  constructor(private fb: AngularFireAuth) {
+    this.authenticated = new Observable<boolean>((obs) => {
+      fb.auth.onAuthStateChanged((u) => {
+        if (u) {
+          this.userId = u.uid;
+          obs.next(true);
+        } else {
+          this.userId = '';
+          obs.next(false);
+        }
+      });
+    });
+  }
+
+  signInWithEmail(email: string, pass: string) {
+    return this.fb.auth.signInWithEmailAndPassword(email, pass);
+  }
+
+  isAuthenticated(): Observable<boolean> {
+    return this.authenticated;
+  }
 }
